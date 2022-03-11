b3dcda15aedd13f876b0ff941e204ee81719e15a
Enrique Jimenez <ejimenez2292@gmail.com>
2022-03-10 22:03:09 -0600

    added issues list

--- assets/js/single.js
+++ assets/js/single.js
@@ -2,6 +2,18 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+
+var getRepoName = function () {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if(repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    document.location.replace("./index.html");
+  }
+}
+
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -20,8 +32,7 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
@@ -82,3 +93,4 @@ var displayWarning = function(repo) {
 };
 
 getRepoIssues("facebook/react");
+getRepoName();
